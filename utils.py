9973690fee777c44fbcedc62def619fc2309e9d3
Evotushon <danielspa2019@gmail.com>
2021-10-20 16:45:17 +0200

    haha enter funny

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from imports import *
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 # Prefisso cambiabile in ogni momento
-prefix = "ev! "
+prefix = "ev!"
 bot = commands.Bot(command_prefix=prefix)
 token = TOKEN
 
